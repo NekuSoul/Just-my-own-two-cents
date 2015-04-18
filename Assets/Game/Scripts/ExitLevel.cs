86cfa70c21d3e8d05d155463e8bcaa9b822ee386
NekuSoul <Git@NekuSoul.de>
2015-04-18 21:25:20 +0200

    Added real Level1

--- Assets/Game/Scripts/ExitLevel.cs
+++ Assets/Game/Scripts/ExitLevel.cs
@@ -1,27 +1,36 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ExitLevel : MonoBehaviour 
+public class ExitLevel : MonoBehaviour
 {
-	public string NextScene;
+    public string NextScene;
+    public AudioClip ExitSound;
+    public AudioSource GlobalAudioSource;
 
-	// Use this for initialization
-	void Start ()
-	{
-	
-	}
-	
-	// Update is called once per frame
-	void Update () 
-	{
-	
-	}
+    // Use this for initialization
+    void Start()
+    {
 
-	void OnTriggerEnter2D(Collider2D other)
-	{
-		if (other.gameObject.GetComponent<PlayerController>() is PlayerController)
-		{
-			Application.LoadLevel(NextScene);
-		}
-	}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.GetComponent<PlayerController>() is PlayerController)
+        {
+            other.gameObject.GetComponent<PlayerController>().enabled = false;
+            GlobalAudioSource.PlayOneShot(ExitSound);
+            Invoke("ChangeLevel", 1f);
+        }
+    }
+
+    void ChangeLevel()
+    {
+        Application.LoadLevel(NextScene);
+    }
 }
